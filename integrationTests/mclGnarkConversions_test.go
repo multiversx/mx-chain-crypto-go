556422239bcbf2dd86ff522e41de76b30b2cdf0e
cosmatudor <tudor.cosma53@gmail.com>
2025-04-25 17:08:47 +0300

    add interop from bls to mcl

--- integrationTests/mclGnarkConversions_test.go
+++ integrationTests/mclGnarkConversions_test.go
@@ -4,12 +4,13 @@ import (
 	"testing"
 
 	"github.com/multiversx/mx-chain-crypto-go/curves/bls/bls12381"
-	"github.com/multiversx/mx-chain-crypto-go/curves/bls/bls12381/interop"
+	blsInterop "github.com/multiversx/mx-chain-crypto-go/curves/bls/bls12381/interop"
 	"github.com/multiversx/mx-chain-crypto-go/signing/mcl"
+	mclInterop "github.com/multiversx/mx-chain-crypto-go/signing/mcl/interop"
 	"github.com/stretchr/testify/require"
 )
 
-func TestFromMCL(t *testing.T) {
+func TestFromMCLToGnark(t *testing.T) {
 	mclSuite := mcl.NewSuiteBLS12()
 	bls12381Suite := bls12381.NewSuiteBLS12()
 
@@ -17,10 +18,28 @@ func TestFromMCL(t *testing.T) {
 		_, pk := mclSuite.CreateKeyPair()
 
 		pointBytes, _ := pk.MarshalBinary()
-		convertedPoint, err := interop.PointBytesFromMcl(pointBytes)
+		convertedPoint, err := blsInterop.PointBytesFromMcl(pointBytes)
 		require.Nil(t, err)
 
 		err = bls12381Suite.CheckPointValid(convertedPoint)
+
+		require.Nil(t, err)
+	}
+}
+
+func TestFromGnarkToMCL(t *testing.T) {
+	gnarkSuite := bls12381.NewSuiteBLS12()
+	mclSuite := mcl.NewSuiteBLS12()
+
+	for i := 0; i < 500; i++ {
+		_, pk := gnarkSuite.CreateKeyPair()
+
+		pointBytes, _ := pk.MarshalBinary()
+		compressedPointBytes := pointBytes[:96]
+		convertedPoint, err := mclInterop.PointBytesFromBLS(compressedPointBytes)
+		require.Nil(t, err)
+
+		err = mclSuite.CheckPointValid(convertedPoint)
 
 		require.Nil(t, err)
 	}
--- signing/mcl/interop/flags.go
+++ signing/mcl/interop/flags.go
@@ -1,12 +1,38 @@
 package interop
 
-import "slices"
+import (
+	"errors"
+	"slices"
+)
 
-func removeFlags(rawPoint []byte) []byte {
-	inPlacePoint := append([]byte(nil), rawPoint...)
+const (
+	g2CompressedSize = 96
+	compressedBit    = 0x80
+	infiniteBit      = 0x40
+	signBit          = 0x20
+)
 
-	slices.Reverse(inPlacePoint)
-	inPlacePoint[0] |= 0x80
+// PointBytesFromBLS converts a point from BLS to MCL format
+func PointBytesFromBLS(rawPoint []byte) ([]byte, error) {
+	if len(rawPoint) != g2CompressedSize {
+		return nil, errors.New("interop: raw BLS point must be 96 bytes")
+	}
 
-	return inPlacePoint
+	be := reverseBytes(rawPoint)
+	removeFlags(be)
+
+	return be, nil
+}
+
+func reverseBytes(in []byte) []byte {
+	out := append([]byte(nil), in...)
+	slices.Reverse(out)
+
+	return out
+}
+
+func removeFlags(buf []byte) {
+	buf[g2CompressedSize-1] &^= compressedBit
+	buf[g2CompressedSize-1] &^= infiniteBit
+	buf[g2CompressedSize-1] &^= signBit
 }
