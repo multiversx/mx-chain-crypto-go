bbf8b968b9ffc57ef7b241b5eddb75fbc3b050ff
robertsasu <sasu.robert@gmail.com>
2025-08-21 16:49:21 +0300

    cleanup constants

--- zk/lowLevelFeatures/constants.go
+++ zk/lowLevelFeatures/constants.go
@@ -7,17 +7,10 @@ import (
 type ID = ecc.ID
 
 const (
-	Unknown    = ecc.UNKNOWN
-	BN254      = ecc.BN254
-	BLS12_377  = ecc.BLS12_377
-	BLS12_381  = ecc.BLS12_381
-	BLS24_315  = ecc.BLS24_315
-	BLS24_317  = ecc.BLS24_317
-	BW6_761    = ecc.BW6_761
-	BW6_633    = ecc.BW6_633
-	StarkCurve = ecc.STARK_CURVE
-	Secp256k1  = ecc.SECP256K1
-	Grumpkin   = ecc.GRUMPKIN
+	Unknown   = ecc.UNKNOWN
+	BN254     = ecc.BN254
+	BLS12_377 = ecc.BLS12_377
+	BLS12_381 = ecc.BLS12_381
 )
 
 // GroupID defines the given group
